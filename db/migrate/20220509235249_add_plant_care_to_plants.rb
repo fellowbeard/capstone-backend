b7326eef6d4e322c7f32a817b5cfdb706399735e
fellowbeard <virgovibe@gmail.com>
2022-05-09 13:57:18 -1000

    adds plantcare to plants and phone number to users

--- db/migrate/20220509235249_add_plant_care_to_plants.rb
+++ db/migrate/20220509235249_add_plant_care_to_plants.rb
@@ -1,4 +1,6 @@
 class AddPlantCareToPlants < ActiveRecord::Migration[7.0]
   def change
+    add_column :plants, :plant_care, :text
+    add_column :users, :phone_number, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_06_015945) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_09_235249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_06_015945) do
     t.string "name"
     t.string "description"
     t.string "image_url"
+    t.text "plant_care"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_06_015945) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
   end
 
 end
