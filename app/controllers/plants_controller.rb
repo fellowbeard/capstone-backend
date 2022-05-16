d15317edd06dc24f1b8eec2fdd5d7e44288dfc26
fellowbeard <virgovibe@gmail.com>
2022-05-16 13:33:44 -1000

    adds plant care to plant controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,6 +2,12 @@ class PlantsController < ApplicationController
 
   def index
     plants = Plant.all
+    pp params[:search]
+    if(params[:search])
+      plants = plants.where("name ILIKE ?", "%#{params[:search]}%")
+    end
+    plants = plants.order(:id => :asc)
+
     render json: plants.as_json
   end
   
